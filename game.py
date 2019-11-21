35540839287934aeabf8d444700d9a0e3618ef5f
y.yamauchi <y.yamauchi@edge-consulting.jp>
2019-11-21 15:47:24 +0900

    fixed

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 from uifiles.main import Ui_MainWindow
 from uifiles.winner import Ui_cong_dialog
+from uifiles.control import Ui_Control
 from PyQt5 import QtCore, QtGui, QtWidgets
 from modules import core
 import sys
@@ -19,23 +20,122 @@ class Winner(Ui_cong_dialog):
         self.dialog.exec_()
         pass
 
+class Control(Ui_Control):
+    def setup(self, dialog, mainwindow):
+        self.main = mainwindow
+        self.dialog = dialog
+        self.pushButton.clicked.connect(self.pushbutton)
+
+        self.startButton.clicked.connect(self.main.button_shuffle)
+        self.labelA_f = False
+        self.labelB_f = False
+        self.labelC_f = False
+        self.labelD_f = False
+        self.labelE_f = False
+        self.labelF_f = False
+        self.labelG_f = False
+        self.labelH_f = False
+        self.labelI_f = False
+        self.labelJ_f = False
+
+        self.teamA_list = self.main.teamA_list
+
+
+
+
+
+
+
+
+
+        self.buttonL.clicked.connect(self.main.button_L)
+        self.buttonR.clicked.connect(self.main.button_R)
+        self.vsButton.clicked.connect(self.main.button_vs)
+        self.nextButton.clicked.connect(self.main.button_next)
+        # self.button_ok.clicked.connect(self.okClicked)
+
+        self.labelA.mousePressEvent = self.labelA_clicked
+        self.labelB.mousePressEvent = self.labelB_clicked
+        self.labelC.mousePressEvent = self.labelC_clicked
+        self.labelD.mousePressEvent = self.labelD_clicked
+        self.labelE.mousePressEvent = self.labelE_clicked
+        self.labelF.mousePressEvent = self.labelF_clicked
+        self.labelG.mousePressEvent = self.labelG_clicked
+        self.labelH.mousePressEvent = self.labelH_clicked
+        self.labelI.mousePressEvent = self.labelI_clicked
+        self.labelJ.mousePressEvent = self.labelJ_clicked
+
+    def labelA_clicked(self, e):
+        self.labelA_f = self.switch_label(self.labelA, self.labelA_f)
+        self.main.labelA_f = self.switch_label(self.main.labelA, self.main.labelA_f)
+    def labelB_clicked(self, e):
+        self.labelB_f = self.switch_label(self.labelB, self.labelB_f)
+        self.main.labelB_f = self.switch_label(self.main.labelB, self.main.labelB_f)
+    def labelC_clicked(self, e):
+        self.labelC_f = self.switch_label(self.labelC, self.labelC_f)
+        self.main.labelC_f = self.switch_label(self.main.labelC, self.main.labelC_f)
+    def labelD_clicked(self, e):
+        self.labelD_f = self.switch_label(self.labelD, self.labelD_f)
+        self.main.labelD_f = self.switch_label(self.main.labelD, self.main.labelD_f)
+    def labelE_clicked(self, e):
+        self.labelE_f = self.switch_label(self.labelE, self.labelE_f)
+        self.main.labelE_f = self.switch_label(self.main.labelE, self.main.labelE_f)
+    def labelF_clicked(self, e):
+        self.labelF_f = self.switch_label(self.labelF, self.labelF_f)
+        self.main.labelF_f = self.switch_label(self.main.labelF, self.main.labelF_f)
+    def labelG_clicked(self, e):
+        self.labelG_f = self.switch_label(self.labelG, self.labelG_f)
+        self.main.labelG_f = self.switch_label(self.main.labelG, self.main.labelG_f)
+    def labelH_clicked(self, e):
+        self.labelH_f = self.switch_label(self.labelH, self.labelH_f)
+        self.main.labelH_f = self.switch_label(self.main.labelH, self.main.labelH_f)
+    def labelI_clicked(self, e):
+        self.labelI_f = self.switch_label(self.labelI, self.labelI_f)
+        self.main.labelI_f = self.switch_label(self.main.labelI, self.main.labelI_f)
+    def labelJ_clicked(self, e):
+        self.labelJ_f = self.switch_label(self.labelJ, self.labelJ_f)
+        self.main.labelJ_f = self.switch_label(self.main.labelJ, self.main.labelJ_f)
+    def switch_label(self, label, flag):
+        if not flag:
+            label.setStyleSheet("background-color:red")
+            return True
+        else:
+            label.setStyleSheet("")
+            return False
+
+
+
+
+
+
+
+    def pushbutton(self):
+        self.main.labelA.setText("AAAAAAAA")
+
+
+    def show(self):
+        self.dialog.show()
+        # self.dialog.exec_()
+
 
 class Main(Ui_MainWindow):
 
 
     def setup(self):
+
+
         self.d = core.csv2dict("data/test_data.csv")
         self.startButton.clicked.connect(self.button_shuffle)
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.shuffle)
         self.buttonL.clicked.connect(self.button_L)
         self.buttonR.clicked.connect(self.button_R)
+        self.vsButton.clicked.connect(self.button_vs)
+        self.nextButton.clicked.connect(self.button_next)
 
         self.textline_timer = QtCore.QTimer()
         self.textline_timer.timeout.connect(self.set_textline)
 
-        self.vsButton.clicked.connect(self.button_vs)
-        self.nextButton.clicked.connect(self.button_next)
 
         self.is_start = False
 
@@ -257,6 +357,8 @@ class Main(Ui_MainWindow):
         team_objects = [self.teamA_list, self.teamB_list, self.teamC_list, self.teamD_list, self.teamE_list,
                         self.teamF_list, self.teamG_list, self.teamH_list, self.teamI_list, self.teamJ_list]
 
+        #?
+        self.team_objects = team_objects
         for key in teams:
             team_objects[team_map.index(key)].clear()
             for t in teams[key]:
@@ -271,5 +373,12 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow)
     ui.setup()
 
-    MainWindow.show()
+    # sub_dialog = QtWidgets.QDialog()
+    # control = Control()
+    # control.setupUi(sub_dialog)
+    # control.setup(sub_dialog, ui)
+    # control.show()
+
+    # MainWindow.show()
+    MainWindow.showFullScreen()
     sys.exit(app.exec_())
